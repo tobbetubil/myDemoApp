26ef219b816e9d97d6f767581a39129d69f364db
Cagdas <cagdas@tutuka.com>
2018-09-24 06:03:53 +0300

    I have started my awesome projectgit statusgit status It will be great.

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static boolean search(ArrayList<Integer> array, int e) {
+        System.out.println("inside search");
+        if (array == null) return false;
+  
+        for (int elt : array) {
+          if (elt == e) return true;
+        }
+        return false;
+      }
+}
